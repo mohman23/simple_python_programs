48859b62426b5c4867a63f1f4b3e3d57ba3e2356
Mohsin Hafeez <47477844+mohman23@users.noreply.github.com>
2023-02-15 19:32:40 +0530

    Update add_all_odd_numbers_between10to20.py

--- add_all_odd_numbers_between10to20.py
+++ add_all_odd_numbers_between10to20.py
@@ -7,14 +7,14 @@ for i in range(10, 20):
         x = x + i
 print(x)
 
-+++++++++++++++++++++++++++++++
+#########################################################
 #another way of doing it
 
 for n in range(10, 21):  #remember range is exclusive, so we have to go up to 21
     if n % 2 != 0:
         x += n
 print(x)        
-+++++++++++++++++++++++++++++++
+#########################################################
 #Instead of looping over every number between 10 and 20, this solution only loops over the odd numbers.  
 #Remember, the 3rd argument to range() is the STEP or interval that you want the range to increment by.
 
