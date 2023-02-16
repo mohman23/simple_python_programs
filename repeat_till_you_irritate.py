41835ce0297b4cebd9af8fb019c58d197fb94567
Mohsin Hafeez <47477844+mohman23@users.noreply.github.com>
2023-02-16 23:10:47 +0530

    Create repeat_till_you_irritate.py

---
+++ repeat_till_you_irritate.py
@@ -0,0 +1,12 @@
+x = input("Hey, how's it going? ")
+print(x)
+
+z = "stop copying me"
+
+while x != z:
+	y = input()
+	print(y)
+
+	if y == z:
+		print("fine, you win, now please stop it!!!")
+		break
