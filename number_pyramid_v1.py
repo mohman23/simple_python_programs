19b11ecbe8019c179dd67a5f8f8c163749152794
Mohsin Hafeez <47477844+mohman23@users.noreply.github.com>
2023-02-16 21:41:05 +0530

    Update number_pyramid_v1.py

--- number_pyramid_v1.py
+++ number_pyramid_v1.py
@@ -1,8 +1,13 @@
 # example
 i = 1
+
 while i <= 4:
     print("1" * i)
     i += 1
+
+#Same but with a for loop.
+for i in range(1, 5):
+	print("1" * i)
 
 #>>> print("1" * 1)
 #1
@@ -10,3 +15,5 @@ while i <= 4:
 #11
 #>>> print("1" * 3)
 #111
+
+
