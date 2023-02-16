fb5e56ed9708be6fe49aaf4043bd3e73f687c434
Mohsin Hafeez <47477844+mohman23@users.noreply.github.com>
2023-02-16 21:22:51 +0530

    Create print_triangle_of_1's.py

---
+++ print_triangle_of_1's.py
@@ -0,0 +1,15 @@
+# example
+for i in range(1, 6):
+    for j in range(5-i):
+        print(" ", end="")
+    for k in range(2*i-1):
+        print("1", end="")
+    print()
+
+#this piece of code will print the below pattern
+
+#    1
+#   111
+#  11111
+# 1111111
+#111111111
