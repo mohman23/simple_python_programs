60cb5da48dcd0736a46a5a34dff070c99de14a82
Mohsin Hafeez <23.mohsin@gmail.com>
2025-04-14 11:50:53 +0530

    Update prime_number.py
    added comments for clarity.

--- prime_number.py
+++ prime_number.py
@@ -1,3 +1,5 @@
+#my working program to check if a number is prime or not.
+
 num = int(input("Please enter a number to check if it's prime or not: "))
 
 print("Number you entered is:", num)
@@ -9,11 +11,14 @@ if num == 1:
 # Check for prime
 elif num > 1:
     for i in range(2, num):
+        #This for loop will keep running until it checks all numbers between 2 and num-1.
         if num % i == 0:
             print("The number you entered isn't a prime number.")
             break
     else:
-        # This else belongs to the for loop, not the if!
+        #This else belongs to the for loop, not the if!
+        #The else attached to a for loop only runs if the loop didn’t break early.
+        #It's a special Python feature which allows else to run with a for loop.
         print("The number you entered is a prime number.")
 else:
     print("Please enter a positive integer greater than 0.")
